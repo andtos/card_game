532e5ff843a9f0d09f451c1716efe9ab8d610fb3
ast8705 <andtoskas1104@gmail.com>
2023-01-24 15:45:22 -0500

    working version without shuffle

--- Game.cpp
+++ Game.cpp
@@ -11,15 +11,17 @@ void Game::startGame() {
     Deck deck;
     deck = deck.initNewDeck();
     deck.shuffle();
+    
+
     player1.drawCard(deck.draw());
     player1.drawCard(deck.draw());
     player2.drawCard(deck.draw());
     player2.drawCard(deck.draw());
-    std::cout << player1.getHand1().getSuit() << " " << player1.getHand1().getValue() << " " << player1.getHand1().getSuit() 
-    << " " << player1.getHand1().getValue()<< " " << player1.handTotal() << " player 1 hand \n";
-     std::cout << player2.getHand1().getSuit() << " " << player2.getHand1().getValue() << " " << player2.getHand1().getSuit() 
-    << " " << player2.getHand1().getValue()<< " " << player2.handTotal() << " player 2 hand \n";
-    std::cout << Game::whoseWinning();
+    std::cout << player1.getHand1().getSuit() << " " << player1.getHand1().getValue() << " " << player1.getHand2().getSuit() 
+    << " " << player1.getHand2().getValue()<< " total: " << player1.handTotal() << " player 1 hand \n";
+     std::cout << player2.getHand1().getSuit() << " " << player2.getHand1().getValue() << " " << player2.getHand2().getSuit() 
+    << " " << player2.getHand2().getValue()<< " total: " << player2.handTotal() << " player 2 hand \n";
+    std::cout << "Winner: player " << Game::whoseWinning();
 }
 
 int Game::whoseWinning(){
--- Player.cpp
+++ Player.cpp
@@ -1,34 +1,36 @@
 #include "Player.hpp"
-#include <iostream>
 
 Player::Player(){
     Card c(-1, "empty");
     Card arr[2];
     arr[0]= c;
+    
     arr[1] = c;
 }
 int Player::handTotal(){
-    return Player::arr[0].getValue() + Player::arr[1].getValue();    
+    return arr[0].getValue() + arr[1].getValue();    
 }
 
 
 void Player::drawCard(Card c){
     
-    if(Player::arr[0].isNull() == 0){
-        Player::arr[0]= c;
+    if(arr[0].isNull() == 1){
+        arr[0]= c;
+       
     }
     else{
-        Player::arr[1] = c;
+        arr[1] = c;
+
     }
-    std::cout << Player::arr[0].getValue() << " draw \n";
+    
 }
 
 Card Player::getHand1(){
    
-    return Player::arr[0];
+    return arr[0];
 }
 Card Player::getHand2(){
-    return Player::arr[1];
+    return arr[1];
 }
 
 
