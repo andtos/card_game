c7968831f7a95a379b134e11ec333236b07b89b7
Marcus Lau <108362808+BabyDinos@users.noreply.github.com>
2023-01-23 20:45:26 -0500

    Window Classes

---
+++ Window.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <iostream>
+#include "wtypes.h"
+#include "TextureManager.hpp"
+
+class Window {
+
+public:
+    //external class functions
+    void getWindowResolution(int& horizontal, int& vertical);
+    void createWindow(const char* title, int posx, int posy, int width, int height, Uint32 flags);
+    void handleEvent(SDL_Event& event);
+    void update(float delta_time);
+    void clean();
+
+    //internal class functions
+
+    //functions for Game
+    SDL_Surface* window_surface;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+
+    SDL_Rect yes_button;
+    SDL_Rect no_button;
+
+    int window_width;
+    int window_height;
+    int window_center_x;
+    int window_center_y;
+    bool escape_box_active = false;
+    bool is_running;
+
+private:
+    int count = 0;
+
+};
